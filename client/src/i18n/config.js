938189ba39495039eedeef3e14e2cc8bf9daad7a
Laxman Gaidhankar <laxmangaidhankar44@gmail.com>
2026-09-19 10:48:12 +0530

    config: language models

---
+++ client/src/i18n/config.js
@@ -0,0 +1,38 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import enCommon from '../locales/en/common.json';
+import hiCommon from '../locales/hi/common.json';
+import mrCommon from '../locales/mr/common.json';
+
+export const resources = {
+  en: {
+    common: enCommon,
+  },
+  hi: {
+    common: hiCommon,
+  },
+  mr: {
+    common: mrCommon,
+  },
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'en',
+    ns: ['common'],
+    defaultNS: 'common',
+    interpolation: {
+      escapeValue: false,
+    },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
+    }
+  });
+
+export default i18n;
